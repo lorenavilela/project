ed28c8b7196e8709ef1c4e4cc62ad939f329a170
Lorena Vilela de Souza <lorena.vilela@gmail.com>
2015-09-27 16:32:44 +1300

    Review files

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 project
 ============================================================================
-Repo created according to the course project - Datascience course.
+Repo created according to the course project - Data Science course.
 ============================================================================
 It contains the documents below:
  - run_analysis.R - script to collect, work, and clean a data set related to Human Activity Recognition Using Smartphones Dataset.
  
- - CodeBook.md - describes the variables in the data set result of the script run_analysis.R. You can find the content in the files - activities_measures.txt and activity.txt.
+ - CodeBook.md - describes the variables in the data set result of the script run_analysis.R. 
+ You can find the content in tidy_data.txt
  Also, it describes the tasks I performed to clean up the data and get the results as requested.
  
  Dataset result of the transformations to clean the data collect as mentioned above:
- - activities_measures.txt - contains the average measure of the features per activity and subject.
- - activity.txt - contains the list of activities referred in the file activities_measures.txt
+ - tidy_data.txt - contains the average measure of the features per activity and subject.
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 ##You should create one R script called run_analysis.R that does the following. 
 
-
 run_analysis <- function(){
     library(dplyr)
     library(data.table)
@@ -37,41 +36,34 @@ run_analysis <- function(){
     ##2-Extracts only the measurements on the mean and standard deviation for each measurement. 
     dt_features <- read.table("../project/UCI HAR Dataset/features.txt")
     mean_std_columns <- grep("mean|std", dt_features[,2], ignore.case = TRUE)
-    dt_std_mean <- dt[mean_std_columns]
-    
+    dt_std_mean <- cbind(dt[mean_std_columns], dt[562:563])
+
     ##3-Uses descriptive activity names to name the activities in the data set
     dt_activities <- read.table("../project/UCI HAR Dataset/activity_labels.txt")
     colnames(dt_activities) <- paste("col", 1:2, sep = "")
-    dt <- merge(dt, dt_activities, by.x = "col562", by.y = "col1", all=TRUE)
+    dt_std_mean <- merge(dt_std_mean, dt_activities, by.x = "col562", by.y = "col1", all=TRUE)
         
     ##4-Appropriately labels the data set with descriptive variable names. 
-    columnsname <- c("activity_id", as.vector(dt_features[,2]), "subject", "activity")
-    colnames(dt) <- columnsname
+    activity_names <- c("activity_id", as.vector(dt_features[mean_std_columns,2]), "subject", "activity")
+    colnames(dt_std_mean) <- activity_names
 
     ##5-From the data set in step 4, creates a second, independent tidy data set with the average 
     ##of each variable for each activity and each subject.
        
         #In tidy data: Each variable forms a column and each observation forms a row.
-        
         #Melting data because the features are mixed they and are columns instead of rows.
-        dtmelt <- melt(dt, id=c("subject", "activity", "activity_id"), measure.vars=2:562)
+        dt_std_mean <- melt(dt_std_mean, id=c("subject", "activity", "activity_id"), measure.vars=2:87)
 
-        #Calculating the average measure by activity and subject(person) 
-        dtmelt <-  dtmelt %>%
+        #Calculating the average measure of each feature by activity and subject 
+        dt_result <-  dt_std_mean %>%
              group_by(subject, 
-                     activity_id, 
-                     activity)%>%
+                     activity,
+                     variable)%>%
             summarize(avg_feature = mean(value))
-        
-        
-        #In tidy data: Each type of observational unit forms a table
-        
-        #Creating the Activity table
-        tb_activity <- unique(dtmelt[,c("activity_id","activity")])
-        
-        #Creating the Average measures by activities and subject table
-        tb_avg_measure_per_activity_subject <- select(dtmelt, subject, activity_id, avg_feature)
-       
+
+        #Adjusting column names
+        column_names <- c("subject_id", "activity", "measurement", "measurement_value")
+        colnames(dt_result) <- column_names
 }
 
 
